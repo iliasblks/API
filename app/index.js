7f0f01e90303a1b9ead94da7f2e2969c8534b047
iliasblks <iliasboulakhras@MSI.localdomain>
2022-11-12 16:42:13 +0100

    First Commit

---
+++ app/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect('', {
+    useNewUrlParser: true
+}, (error) => {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log('BD connect !');
+    }
+})
+
+const classesRouter = require('./routes/classes');
+
+app.get("/", (req, res) => {
+    res.status(200).send('<h1>Hello World !</h1>');
+});
+
+app.use('/classes', classesRouter);
+
+app.listen(4500, () => {
+    console.log('Server is running on http://127.0.0.1:4500');
+});
---
+++ app/routes/classes.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const { v4: uuidv4 } = require('uuid');
+
+let router = express.Router();
+
+let classes = [];
+
+router.post('/', (request, response) => {
+    const {name} = request.body;
+
+    let classe = {
+        id: uuidv4(),
+        name
+    };
+
+
+    classes.push(classe);
+    response.status(200).json(classe);
+});
+
+router.get('/', (request, response) => {
+    response.status(200).json(classes);
+});
+
+router.get('/:id', (request, response) => {
+    const {id} = request.params;
+
+    let classe = classes.find(item => item.id === id);
+
+    response.status(200).json(classe);
+});
+
+router.delete('/:id', (request, response) => {
+    const {id} = request.params;
+
+    classes = classes.filter(object => { return object.id !== id; });
+
+    response.status(200).json(classes);
+});
+
+router.put('/:id', (request, response) => {
+    const {id} = request.params;
+    const {name} = request.body;
+
+    let classe = classes.find(item => item.id === id);
+
+    classe.name = name;
+
+    response.status(200).json(classe);
+})
+
+module.exports = router;
